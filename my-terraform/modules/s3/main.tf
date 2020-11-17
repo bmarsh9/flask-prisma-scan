3781cc2aeec2fc1a9977cf03af62087ee62d7f10
bmarsh9 <bmarshall735@gmail.com>
2020-11-17 16:35:32 -0500

    edit tf main

--- my-terraform/modules/s3/main.tf
+++ my-terraform/modules/s3/main.tf
@@ -21,10 +21,10 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_public_access_block" "bucket" {
   bucket = element(var.buckets, count.index)
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 
   count = length(var.buckets)
 }
